fe496199ff2a6207fa17126fe25dd29db8858fe1
teirm <tribunus.vii@gmail.com>
2016-11-19 11:27:43 -0500

    Added basic cli

---
+++ project_2/tomsim.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+"""
+Project: tomsim simulator
+Course:  CS2410
+Author:  Cyrus Ramavarapu
+Date:    19 November 2016
+"""
+
+import sys
+import json
+
+def tomsim(trace_file, config_file, output_file):
+    """Simulates Tomasulos on the given trace
+       based on the information in the configuration
+       file and outputs statistics to the output_file
+
+       Keyword arguments:
+       trace_file -- trace file of X encoded instructions
+       config_file -- defines system set up
+       output_file -- output json file
+
+       Return: None
+    """
+    print(trace_file)
+    print(config_file)
+    print(output_file)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 4:
+        print('Usage: ./tomsim tracefile config.json output.json')
+        exit(1)
+
+    TRACE_FILE = sys.argv[1]
+    CONFIG_FILE = sys.argv[2]
+    OUTPUT_FILE = sys.argv[3]
+
+    tomsim(TRACE_FILE, CONFIG_FILE, OUTPUT_FILE)
