5a91159211bab8e7859c7320c944ee22f118eaf8
teirm <tribunus.vii@gmail.com>
2016-11-23 10:36:03 -0500

    Wrote handlers for events

--- project_2/tomsim.py
+++ project_2/tomsim.py
@@ -93,7 +93,7 @@ class PipeEvent:
            Location:    {}
            Position:    {}
            """.format(self.event,
-                      self.instruction, 
+                      self.instruction,
                       self.dest,
                       self.source_1,
                       self.source_2,
@@ -101,6 +101,16 @@ class PipeEvent:
                       self.end,
                       self.location,
                       self.position)
+
+    def get_instruction(self):
+        """Returns the instruction type of the event
+
+        Keyword arguments:
+        None
+
+        Returns: String
+        """
+        return self.instruction
 
     def set_sources(self, s1, s2):
         """Sets the sources for the event
@@ -136,18 +146,17 @@ class PipeEvent:
         Returns: None
         """
         self.dest = dest
-    
+
     def get_destination(self):
         """Gets the FU for the event
-        
+
         Keyword arguments:
         None
-        
+
         Returns: None
         """
-        return self.dest 
-    
-        
+        return self.dest
+
     def update_event(self, new_event):
         """Updates the event as it moves through
         the event queue
@@ -178,9 +187,8 @@ class PipeEvent:
         new_end -- new end time
 
         Returns: None
-        """ 
+        """
         self.end == new_end
-
 
     def get_event(self):
         """Returns the event type
@@ -339,6 +347,27 @@ class FunctionalUnit:
                        self.status,
                        self.end_cycle,
                        self.destination)
+
+    def set_status(self, status):
+        """Sets the status of a functional unit
+
+        Keyword arguments:
+        status -- the status to set a functional unit to
+                  either BUSY or FREE
+
+        Returns
+        None
+        """
+        self.status = status
+
+    def get_status(self):
+        """Gets the status of a functional unit
+
+        Keyword arguments:
+        None
+
+        Return: Int FREE (0) or BUSY (1)
+        """
 
     def get_statistics(self):
         """Gets the statistics for the functional unit
@@ -622,10 +651,11 @@ def get_resv_station(op_name):
 
 # EVENT HANDLERS
 
+
 def write_op_handler(current_cycle):
     """Handles WRITE_OPERAND events in the event queue.
     If an event is found that can be processed on the
-    current clock cycle it will set the RS_STATUS 
+    current clock cycle it will set the RS_STATUS
     renamed register to True (1) and make the FU free
     (NOT BUSY).  The event is then removed from the
     EVENT QUEUE.
@@ -636,23 +666,23 @@ def write_op_handler(current_cycle):
 
     Returns: Int indicating the number of events found
              and processed.
-    """ 
+    """
 
     events_processed = 0
     queue_position = 0
 
     for event in EVENT_QUEUE:
-        if event.get_end() == current_cycle and event.get_event() == 'WO':            
-            event_name = event.get_event() 
-            fu_destination = event.get_dest() 
-            (location, position) = event.get_resv_info()          
-            #Update REG_RENAME
-            #free FU for event_name and RS
-            EVENT_QUEUE.remove(queue_position)      
+        if event.get_end() == current_cycle and event.get_event() == 'WO':
+            event_name = event.get_event()
+            fu_destination = event.get_dest()
+            (location, position) = event.get_resv_info()
+            # Update REG_STATUS
+            # free FU for event_name and RS
+            EVENT_QUEUE.remove(queue_position)
             events_processed += 1
-             
-        queue_position += 1              
-    
+
+        queue_position += 1
+
     return events_processed
 
 
@@ -670,33 +700,136 @@ def exec_handler(current_cycle):
     Returns: Int indiciatng the number of events found
              and processed.
     """
-    
+
     events_processed = 0
-    
+
     for event in EVENT_QUEUE:
         if event.get_end() == current_cycle and event.get_event() == 'EXEC':
             event.update_event('WO', current_cycle, current_cycle + 1)
             event.update_start(current_cycle)
-            event_update_end(current_cycle + 1)
-    
+            event.update_end(current_cycle + 1)
+
             events_processed += 1
 
     return events_processed
+
+
+def check_sources(s1, s2):
+    """Checks the status of the renamed registers
+    to determine if a queued event can proceed
+    from RO to EXEC with the necessary sources
+
+    Keyword arguments:
+    s1 -- source operand 1
+    s2 -- source operand 2
+
+    Return: Tuple with (s1 status, s2 status)
+    """
+    s1_status = RES_STATUS[s1]
+    s2_status = RES_STATUS[s2]
+
+    return (s1_status, s2_status)
+
+
+def find_func_unit(instr):
+    """Finds an available functional unit for
+    the ready instruction.
+
+    Keyword argument:
+    instr -- the type of instruction being run
+
+    Return: (Int, Int) indicating position of FU marked busy
+            and its latency. (-1,-1) if none found
+    """
+
+    int_operations = [
+        'add',
+        'sub',
+        'nor',
+        'and',
+        'lis',
+        'liz',
+        'lui',
+        'put',
+        'halt']
+    div_operations = ['div', 'exp', 'mod']
+
+    fu_position = 0
+
+    if instr in int_operations:
+        for int_fu in INTEGER:
+            if int_fu.get_status() == FREE:
+                int_fu.set_status(BUSY)
+                return fu_position
+
+            fu_position += 1
+    elif instr in div_operations:
+        for div_fu in DIVIDER:
+            if div_fu.get_status() == FREE:
+                div_fu.set_status(BUSY)
+                return fu_position
+
+        fu_position += 1
+    elif instr is 'mul':
+        for mul_fu in MULTIPLIER:
+            if mul_fu.get_status() == FREE:
+                mul_fu.set_status(BUSY)
+                return fu_position
+
+        fu_position += 1
+
+    elif instr is 'lw':
+        for ld_fu in LOAD:
+            if ld_fu.get_status() == FREE:
+                ld_fu.set_status(BUSY)
+                return fu_position
+
+        fu_position += 1
+
+    else:
+        for sw_fu in STORE:
+            if sw_fu.get_status() == FREE:
+                sw_fu.set_status(BUSY)
+                return fu_position
+
+        fu_position += 1
+
+    return -1
 
 
 def read_op_handler(current_cycle):
     """Handles READ OPERAND events in the event queue.
        If a read operand event is found, the necessary
        sources are checked for availability.  If both
-       are already available, a functional unit is 
+       are already available, a functional unit is
        checked for availablity and assigned if possible.
        If sources are unavailable or FU not available,
        event is updated as RO with only the end cycle
-       updated. 
+       updated.
+
+    Keyword arguments:
+    current_cycle -- the current cycle the simulation is
+                     processing
+
+    Returns: Int indiciatng the number of events found
+             and processed.
     """
-    
-        
-                        
+
+    for event in EVENT_QUEUE:
+        if event.get_event() == 'RO':
+            # NEED TO FIND OLDEST FOR A GIVEN FU
+            if event.get_end() == current_cycle:
+                (s1, s2) = event.get_sources()
+                (s1_status, s2_status) = check_sources(s1, s2)
+                if (s1_status, s2_status) == (1, 1):
+                    pos = find_func_unit(event.get_instruction())
+                    event.update_event('EXEC')
+                    event.update_start(current_cycle)
+                    # MUST GET LATENCY OF FU
+                    event.update_end(current_cycle + 1)
+                else:
+                    event.update_end(current_cycle + 1)
+
 
 def tomsim(trace_file, config_file, output_file):
     """Simulates Tomasulos on the given trace
@@ -720,15 +853,15 @@ def tomsim(trace_file, config_file, output_file):
 #    print(instructions)
 
     while not halt_sig:
-       
-        # Only if pipeline is NOT STALLED  
+
+        # Only if pipeline is NOT STALLED
         (name, dest, s1, s2) = get_instruction(instructions, instruction_count)
         (res_name, res_pos) = get_resv_station(name)
         instruction_count += 1
-       
-        #NEED FUNCTION TO HANDLE WRITE_OPS
-        #NEED FUNCTION TO HANDLE EXECUTES 
-        #NEED FUNCTION TO HANDLE READ_OPS
+
+        # NEED FUNCTION TO HANDLE WRITE_OPS
+        # NEED FUNCTION TO HANDLE EXECUTES
+        # NEED FUNCTION TO HANDLE READ_OPS
 
         if res_name is not None:
             new_event = PipeEvent('READ_OPERAND', name, clock_cycle, 1)
